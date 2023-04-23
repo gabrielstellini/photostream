e964d380fa1f67263d371e305e3d8a85603b31d1
Gabriel Stellini <gabriel.stellini@fusonic.net>
2023-04-23 12:05:35 +0200

    Add material

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -5,7 +5,9 @@ import {
 } from '@angular/router';
 import { appRoutes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), importProvidersFrom(BrowserAnimationsModule)],
 }).catch((err) => console.error(err));
