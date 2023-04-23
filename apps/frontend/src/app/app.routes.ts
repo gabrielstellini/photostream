13c749551c0b4a04a2b45608b40300f785242f4f
Gabriel Stellini <gabriel.stellini@fusonic.net>
2023-04-23 08:53:53 +0200

    Add lazy loading standalone component example

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'dashboard',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (mod) => mod.DashboardComponent
+      ),
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'dashboard' },
+];
---
+++ apps/frontend/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nestangular-template-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  standalone: true,
+})
+export class DashboardComponent {}
