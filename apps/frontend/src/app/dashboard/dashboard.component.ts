2af0d23887821316394f939f2d9eae66a5165946
Gabriel Stellini <gabriel.stellini@fusonic.net>
2023-04-23 14:23:47 +0200

    Add more theming

--- apps/frontend/src/app/dashboard/dashboard.component.ts
+++ apps/frontend/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'nestangular-template-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   standalone: true,
+  imports: [MatButtonModule],
 })
 export class DashboardComponent {}
