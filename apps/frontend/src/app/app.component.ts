e9608b257bcb9e76a24bae9aaf383c1568b8f520
Gabriel Stellini <gabriel.stellini@fusonic.net>
2023-04-23 08:38:25 +0200

    Fix issues with build, add run-all command

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, CommonModule],
   selector: 'nestangular-template-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
