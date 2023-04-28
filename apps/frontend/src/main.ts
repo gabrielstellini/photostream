6d51e58a0894f56448721d11cba490b3c2ff0838
Gabriel Stellini <gabriel.stellini@fusonic.net>
2023-04-28 09:24:27 +0200

    Add ngrx

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -7,7 +7,14 @@ import { appRoutes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), importProvidersFrom(BrowserAnimationsModule)],
+  providers: [
+    provideEffects(),
+    provideStore(),
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(BrowserAnimationsModule),
+  ],
 }).catch((err) => console.error(err));
