ed8572c6240135568454e781bb26b52643a16992
Gabriel Stellini <gabriel.stellini@fusonic.net>
2023-04-29 12:34:26 +0200

    Add tests to toolbar and photolist component

--- apps/frontend/src/app/shared/photo-list-item/photo-list-item.component.spec.ts
+++ apps/frontend/src/app/shared/photo-list-item/photo-list-item.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotoListItemComponent } from './photo-list-item.component';
+import { PhotoDto } from '../../../store/photo/types/photos.model';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 describe('PhotoListItemComponent', () => {
   let component: PhotoListItemComponent;
@@ -8,9 +11,12 @@ describe('PhotoListItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ PhotoListItemComponent ]
+      imports: [PhotoListItemComponent, RouterTestingModule],
     })
-    .compileComponents();
+      .overrideComponent(PhotoListItemComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(PhotoListItemComponent);
     component = fixture.componentInstance;
@@ -19,5 +25,53 @@ describe('PhotoListItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the photo details if item is provided', () => {
+    const item: PhotoDto = {
+      id: '1',
+      author: 'John Doe',
+      url: 'https://example.com/image.jpg',
+      download_url: 'https://example.com/image-download.jpg',
+      height: 100,
+      width: 100,
+    };
+    component.item = item;
+
+    fixture.detectChanges();
+    fixture.detectChanges();
+    fixture.detectChanges();
+    fixture.detectChanges();
+
+    const cardElement = fixture.nativeElement.querySelector('mat-card');
+    expect(cardElement).toBeTruthy();
+
+    const titleElement = fixture.nativeElement.querySelector(
+      '[data-cy="author"]'
+    );
+    expect(titleElement.textContent.trim()).toBe(item.author);
+
+    const subtitleElement =
+      fixture.nativeElement.querySelector('mat-card-subtitle');
+    expect(
+      subtitleElement
+        .querySelector('[data-cy="source-link"]')
+        .getAttribute('href')
+    ).toBe(item.url);
+
+    const imageElement = fixture.nativeElement.querySelector('img');
+    expect(imageElement.getAttribute('src')).toBe(item.download_url);
+
+    const imageLink = fixture.nativeElement.querySelector('.image-link');
+    expect(imageLink.getAttribute('href')).toBe('/photos/1');
+
+    const spinnerElement = fixture.nativeElement.querySelector('mat-spinner');
+    expect(spinnerElement).toBeFalsy();
+  });
+
+  it('should render a spinner if item is not provided', () => {
+    fixture.detectChanges();
+    const spinnerElement = fixture.nativeElement.querySelector('mat-spinner');
+    expect(spinnerElement).toBeTruthy();
   });
 });
--- apps/frontend/src/app/shared/toolbar/toolbar.component.spec.ts
+++ apps/frontend/src/app/shared/toolbar/toolbar.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ToolbarComponent } from './toolbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
@@ -7,7 +12,14 @@ describe('ToolbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ToolbarComponent],
+      imports: [
+        MatButtonModule,
+        RouterTestingModule,
+        MatToolbarModule,
+        MatIconModule,
+        RouterLinkActive,
+        ToolbarComponent,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ToolbarComponent);
@@ -17,5 +29,28 @@ describe('ToolbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a link to the home page', () => {
+    const link = fixture.nativeElement.querySelector('a');
+    expect(link).toBeTruthy();
+    expect(link.getAttribute('routerLink')).toBe('/');
+    expect(link.textContent.trim()).toBe('PhotoStream');
+  });
+
+  it('should have a link to the photos page', () => {
+    const link = fixture.nativeElement.querySelector('[data-cy="photo-link"]');
+    expect(link).toBeTruthy();
+    expect(link.getAttribute('routerLink')).toBe('/photos');
+    expect(link.textContent.trim()).toBe('Photos');
+  });
+
+  it('should have a link to the favourites page', () => {
+    const link = fixture.nativeElement.querySelector(
+      '[data-cy="favourites-link"]'
+    );
+    expect(link).toBeTruthy();
+    expect(link.getAttribute('routerLink')).toBe('/favourites');
+    expect(link.textContent.trim()).toBe('Favourites');
   });
 });
